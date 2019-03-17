20f3ec786e889ef7a7adf58ad94385d6fe53e675
wernerolaf <o.werner@student.mini.pw.edu.pl>
2019-03-17 13:08:47 +0100

    Add files via upload

--- analiza.R
+++ analiza.R
@@ -22,14 +22,25 @@ heloc_clean$NoTrades<-!no8
 heloc_clean[-1]<-apply(heloc_clean[-1],c(1,2),function(x){ ifelse(x %in% c(-7,-8),NA,x)})
 heloc_clean$MaxDelq2PublicRecLast12M<-as.factor(heloc_clean$MaxDelq2PublicRecLast12M)
 heloc_clean$MaxDelqEver<-as.factor(heloc_clean$MaxDelqEver)
+heloc_clean$NumTrades60Ever2DerogPubRec<-as.factor(heloc_clean$NumTrades60Ever2DerogPubRec)
+heloc_clean$NumTrades90Ever2DerogPubRec<-as.factor(heloc_clean$NumTrades90Ever2DerogPubRec)
+
 xd<-factor(sapply(as.numeric(heloc_clean$MaxDelqEver),function(x){ifelse(x %in% c(1,2,7,9),NA,x)}))
-
-
-treat<-vtreat::designTreatmentsC(heloc_clean,names(heloc_clean)[-1],"RiskPerformance","Good")
+#MaxDelqEver na factory trzeba zamienić
+set.seed(1)
+treat<-vtreat::designTreatmentsC(heloc_clean,names(heloc_clean)[-1],"RiskPerformance","Good",rareCount=dim(heloc_clean)[1]/20)
 scols <- c('varName','sig','extraModelDegrees','origName','code')
 print(treat$scoreFrame[, scols])
 test<-prepare(treat,heloc_clean,pruneSig = 1/length(treat$scoreFrame$varName))
 
+regr_task = makeClassifTask(id = "lvr", data = test, target = "RiskPerformance")
+regr_lrn = makeLearner("classif.rpart",predict.type = "response")
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- resample(regr_lrn, regr_task, cv,measures = acc)
+MSE <- r$aggr
+MSE
 
 #create_report(good)
 #create_report(bad)
