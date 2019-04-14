ebae4c2f8f26fe161fe4e1f80b446cb4fe6f00ea
wernerolaf <olafowerner@gmail.com>
2019-04-14 19:53:21 +0200

    tuning

---
+++ tuning.R
@@ -0,0 +1,18 @@
+
+library(mlr)
+heloc_ok<-read.csv("heloc_ok.csv")
+classif_task <- makeClassifTask( data = heloc_ok, target = "RiskPerformance")
+rf_pars <- tuneParams(
+  
+  makeLearner("classif.randomForest", predict.type = "prob"),
+  subsetTask(classif_task),
+  resampling = cv5,
+  measures = mlr::auc,
+  par.set = makeParamSet(
+    makeDiscreteParam("ntree", values = 100:1000),
+    makeDiscreteParam("mtry", values = 10:50),
+    makeDiscreteParam("nodesize", values = seq(1, 50, by = 5))
+  ),
+  control = makeTuneControlRandom(maxit = 100)
+  
+)
